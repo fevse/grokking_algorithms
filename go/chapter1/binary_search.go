66a1151bcf95d69c0697336bb825e8db5db44887
fevse <fedulov.v.s@gmail.com>
2024-06-15 00:56:55 +0400

    add go examples

---
+++ go/chapter1/binary_search.go
@@ -0,0 +1,20 @@
+package binarysearch
+
+func binarySearch(data []int, item int) (int, int) {
+	var low, high, step int
+	high = len(data)-1
+	for {
+		if low > high {
+			return 0, 0
+		}
+		step += 1
+		mid := (low + high) / 2
+		if data[mid] == item {
+			return mid, step
+		} else if data[mid] > item {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+}
---
+++ go/chapter1/binary_search_test.go
@@ -0,0 +1,30 @@
+package binarysearch
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBinarySearch(t *testing.T) {
+	tests := []struct {
+		data []int
+		item int
+		index int
+		step int
+	}{
+		{data: []int{1, 2, 3}, item: 2, index: 1, step: 1},
+		{data: []int{1, 2, 3}, item: 1, index: 0, step: 2},
+		{data: []int{1, 2, 3, 4, 5, 6}, item: 4, index: 3, step: 3},
+		{data: []int{}, item: 1, index: 0, step: 0},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run("test", func(t *testing.T) {
+			i, s := binarySearch(tc.data, tc.item)
+			require.Equal(t, tc.index, i)
+			require.Equal(t, s, tc.step)
+		})
+	}
+}
