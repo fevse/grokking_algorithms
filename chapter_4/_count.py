9a6f7847a23fe6c77e627a39d76dfa6e377c4ab8
fevse <fedulov.v.s@gmail.com>
2024-06-15 00:57:29 +0400

    some changes

--- chapter_4/_count.py
+++
@@ -1,5 +0,0 @@
-def _count(arr):
-    if len(arr) == 1:
-        return 1
-    else:
-        return 1 + _count(arr[1:])
--- chapter_4/_sum.py
+++
@@ -1,5 +0,0 @@
-def _sum(arr):
-    if len(arr) == 1:
-        return arr[0]
-    else:
-        return arr[0] + _sum(arr[1:])
